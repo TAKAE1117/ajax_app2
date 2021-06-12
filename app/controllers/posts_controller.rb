36399ca1a4fb46824129aed14bbec99f480a5495
TAKAE1117 <takayuki.ogawa.0712@gmail.com>
2021-06-12 11:19:59 +0900

    definition of checked action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,4 @@
 class PostsController < ApplicationController
-
   def index
     @posts = Post.all.order(id: "DESC")
   end
@@ -7,5 +6,17 @@ class PostsController < ApplicationController
   def create
     Post.create(content: params[:content])
     redirect_to action: :index
+  end
+
+  def checked
+    post = Post.find(params[:id])
+    if post.checked               #既読であるか否かを判定するプロパティを指定する
+      post.update(checked: false) #既読を解除するためにfalseへ変更
+    else
+      post.upate(checked: true)   #既読にするためtrueへ変更
+    end
+
+    item = Post.find(params[:id]) #レコードを取得
+    render json: { post: item }   #JSON形式としてchecked.jsに返却
   end
 end
